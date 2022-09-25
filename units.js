b9c70116ed6541f77655b606f619f2d57301e7bc
Adam Rowton <105441680+adam-rowton@users.noreply.github.com>
2022-09-24 22:12:52 -0700

    Updated Styling

--- units.js
+++ units.js
@@ -1,3 +1,15 @@
 // 1 meter = 3.281 feet
 // 1 liter = 0.264 gallon
 // 1 kilogram = 2.204 pound
+
+const btn = document.getElementById("btn")
+
+const input = document.getElementById("input")
+
+btn.addEventListener("click", function() {
+    console.log("click")
+})
+
+input.addEventListener("click", function() {
+    console.log("click")
+})
