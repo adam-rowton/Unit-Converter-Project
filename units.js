722a2473cc90ea1a38099be163aaae08d588d70d
Adam Rowton <105441680+adam-rowton@users.noreply.github.com>
2022-09-25 11:29:47 -0700

    Added js and more styling

--- units.js
+++ units.js
@@ -6,10 +6,34 @@ const btn = document.getElementById("btn")
 
 const input = document.getElementById("input")
 
-btn.addEventListener("click", function() {
-    console.log("click")
-})
 
-input.addEventListener("click", function() {
-    console.log("click")
-})
+const volume = document.getElementById("volume")
+const length = document.getElementById("length")
+const mass = document.getElementById("mass")
+
+
+
+
+
+function setValue() {
+    const userValue = input.value
+    console.log(userValue)
+
+    // formula equations
+
+    const meters = (userValue / 3.281).toFixed(2)
+    const feet = (userValue * 3.281).toFixed(2)
+
+    const liters = (userValue * 0.264).toFixed(2)
+    const gallons = (userValue / 0.264).toFixed(2)
+
+    const kilos = (userValue * 2.204).toFixed(2)
+    const pounds = (userValue / 2.204).toFixed(2)
+
+
+    // DOM manipulation
+
+    length.innerHTML = `${userValue} meters = ${feet} feet | ${userValue} feet = ${meters} meters`
+    volume.innerHTML = `${userValue} liters = ${gallons} gallons | ${userValue} gallons = ${liters} liters`
+    mass.innerHTML = `${userValue} kilograms = ${pounds} pounds | ${userValue} pounds = ${kilos} kilograms`
+}
